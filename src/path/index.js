1b4d8960dc00c4ac9d0fb3641bd4529c177495ea
phuchoa <phuchoa1202@gmail.com>
2023-09-04 16:46:09 +0700

    update

--- src/path/index.js
+++ src/path/index.js
@@ -56,7 +56,7 @@ const Path = [
     populates: [],
     isAdmin: false,
     isLogin: false,
-    fieldSearch: ["username", "email"],
+    fieldSearch: ["courseName", "name"],
     allowPublic: false
   },
   {
@@ -65,7 +65,7 @@ const Path = [
     populates: [],
     isAdmin: false,
     isLogin: false,
-    fieldSearch: ["username", "email"],
+    fieldSearch: ["courseName", "name"],
     allowPublic: false
   },
   {
